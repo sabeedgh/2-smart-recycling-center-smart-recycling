8f2cac2f7a32a926863d1c062434581e34869973
amine <mohamedamine.debbich@esprit.tn>
2023-11-15 02:06:49 +0100

    dialog

---
+++ log.h
@@ -0,0 +1,22 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class log;
+}
+
+class log : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit log(QWidget *parent = nullptr);
+    ~log();
+
+private:
+    Ui::log *ui;
+};
+
+#endif // LOG_H
