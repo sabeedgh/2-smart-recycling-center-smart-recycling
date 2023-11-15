b1e51282380cfb567ffa57923d4913e944e0c542
amine <mohamedamine.debbich@esprit.tn>
2023-11-15 02:04:55 +0100

    connection

---
+++ connection.cpp
@@ -0,0 +1,18 @@
+
+#include "connection.h"
+
+Connection::Connection()
+{}
+bool Connection::createconnect()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("Source_projet2A");
+db.setUserName("amin");//inserer nom de l'utilisateur
+db.setPassword("amine");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+return test;
+}
+void Connection::closeconnect(){db.close();}
+
